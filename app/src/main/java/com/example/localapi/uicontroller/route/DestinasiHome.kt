89e90dbc5bb2cd5f1a02c2081aa6f3b7a9245d38
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-12-26 01:31:44 +0700

    Menambahkan objek DestinasiHome untuk navigasi halaman utama

--- app/src/main/java/com/example/localapi/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/localapi/uicontroller/route/DestinasiHome.kt
@@ -2,6 +2,7 @@ package com.example.localapi.uicontroller.route
 
 import com.example.localapi.R
 
+
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
