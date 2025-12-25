f94add1541011930930462dc6eb72d3661fca509
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-12-26 01:30:03 +0700

    Menambahkan route navigasi untuk halaman entry siswa

--- app/src/main/java/com/example/localapi/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/localapi/uicontroller/route/DestinasiEntry.kt
@@ -2,6 +2,7 @@ package com.example.localapi.uicontroller.route
 
 import com.example.localapi.R
 
+
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
