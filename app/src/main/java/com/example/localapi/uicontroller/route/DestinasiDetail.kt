a75e2071134868f5941d3de9ecc56b8acdbc0fe5
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-12-26 01:13:43 +0700

    Menambahkan destinasi navigasi untuk halaman detail siswa

--- app/src/main/java/com/example/localapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localapi/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,11 @@
 package com.example.localapi.uicontroller.route
 
+import com.example.localapi.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
